486450496e4f7440cb7e1f965402aca759dfecbb
nkenchor <nkenchor@osemeke.com>
2023-09-10 22:56:25 +0100

    Modified: Port

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from app import create_app
 if __name__ == '__main__':
     print("Application has started!")
     app = create_app()
-    app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
 
