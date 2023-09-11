a717ddcea950ee7147fd7c5995ecd595eae8f61b
nkenchor <nkenchor@osemeke.com>
2023-09-11 10:34:18 +0100

    Updated: Moded Loading for suggestions

--- app/application/services/retrieval_services/product_suggestion_service.py
+++ app/application/services/retrieval_services/product_suggestion_service.py
@@ -2,7 +2,8 @@
 from app.application.services.retrieval_services import google_vector_loader
 
 def get_suggestions(word, topn=10):
-    model = google_vector_loader.LOADED_MODEL  # get the model using lazy-loading method
+    model = google_vector_loader.load_model()  # get the model using lazy-loading method
+    
     try:
         # First, try to get synonyms for the word/phrase as a whole
         return [item[0].lower() for item in model.similar_by_word(word, topn=topn)]
