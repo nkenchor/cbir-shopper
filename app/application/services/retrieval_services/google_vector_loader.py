8b42566da3b677eef4ee5af3a5ec29f5bd1d09a2
nkenchor <nkenchor@osemeke.com>
2023-09-11 12:19:39 +0100

    Handled: Model Loading for Google

--- app/application/services/retrieval_services/google_vector_loader.py
+++ app/application/services/retrieval_services/google_vector_loader.py
@@ -18,27 +18,21 @@ def download_googlenews_vector(base_directory=BASE_DIR):
     # Initialize the ModelDownloader with the 'model/pretrained' folder path
     downloader = ModelDownloader(base_dir=base_directory, model_folder=os.path.join(BASE_DIR, 'model', 'pretrained'))
     downloader.download(NLP_NAME, GOOGLENEWS_VECTORS_URL)
-    LOADED_MODEL = load_pretrained_google_model(MODEL_PATH)
+    LOADED_MODEL = load_google_model()
 
-def load_pretrained_google_model():
-    global LOADED_MODEL
-    
-    if LOADED_MODEL is not None:
-        return LOADED_MODEL
-
+def load_google_model():
     try:
-        LOADED_MODEL = gensim.models.KeyedVectors.load_word2vec_format(MODEL_PATH, binary=True)
-        return LOADED_MODEL
+        return gensim.models.KeyedVectors.load_word2vec_format(MODEL_PATH, binary=True)
     except Exception as e:
-        raise Exception(f"Error loading model: {e}")
-
+        print(f"Error loading model: {e}")
+        return None
 
 def get_or_load_nlp_model():
     global LOADED_MODEL
     if not model_exists():
         download_googlenews_vector()
     if LOADED_MODEL is None:
-        LOADED_MODEL = load_pretrained_google_model()
+        LOADED_MODEL = load_google_model()
     return LOADED_MODEL
 
 if __name__ == "__main__":
